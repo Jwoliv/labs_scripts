b66a0a22581bbd5ab3f18a48fb5054371eac7991
Jwoliv <bohdanlishchuk0@gmail.com>
2025-02-22 18:08:45 +0200

    modify script lsb

--- lab1/main.py
+++ lab1/main.py
@@ -5,7 +5,8 @@ from utils import read_jpg, write_jpg, plot_histogram, mse
 
 # Основне виконання
 if __name__ == '__main__':
-    image = read_jpg("images/image_2.jpg")  # Читаємо зображення з файлу
+    image_name = "image_1"
+    image = read_jpg(f"images/{image_name}.jpg")  # Читаємо зображення з файлу
     message = """
         Lorem Ipsum is simply dummy text of the printing and typesetting industry.
         Lorem Ipsum has been the industry's standard dummy text ever since the 1500s...
@@ -13,11 +14,11 @@ if __name__ == '__main__':
 
     # --- Кодування ---
     image_seq = embed_sequential(image, message)
-    write_jpg(f"./images/image_seq_{datetime.now().strftime('%Y_%m_%d_%H_%M_%S')}.jpg", image_seq)
+    write_jpg(f"./images/{image_name}_seq_{datetime.now().strftime('%Y_%m_%d_%H_%M_%S')}.jpg", image_seq)
 
     key = 340698234968  # Ключ для випадкового вбудовування
     image_rand = embed_random(image, message, key)
-    write_jpg(f"./images/image_random_{datetime.now().strftime('%Y_%m_%d_%H_%M_%S')}.jpg", image_rand)
+    write_jpg(f"./images/{image_name}_random_{datetime.now().strftime('%Y_%m_%d_%H_%M_%S')}.jpg", image_rand)
 
     # --- Декодування ---
     retrieved_msg_seq = extract_message(image_seq, len(message))
